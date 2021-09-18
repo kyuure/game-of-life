658418407d68280af5dd64a03de5b7cd5a8ea71e
Salsabila Qothrunnada <salsabila.q.interns@aux.gojek.com>
2021-09-18 21:48:23 +0700

    feat(animations): config canvas and add some variables

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -12,15 +12,38 @@ public class GameOfLife extends javax.swing.JPanel {
   // I forgot what this variable used for...
   private static final long serialVersionUID = 1L;
 
+  // For canvas variable. This variable will be declared publicly so that main
+  // function can access it.
+  public static Image buffer;
+  public static Graphics bg;
+  // Window and canvas size
+  private int windowX = 750;
+  private int windowY = 500;
+
   // GameOfLife class
   public GameOfLife() {
-    // 
+    // Set size for canvas (the same as window size).
+    setPreferredSize(new Dimension(windowX, windowY));
   }
 
   // For canvas
   @Override
   public void paintComponent(Graphics g) {
-    // 
+    // Create new canvas.
+    buffer = createImage(windowX, windowY);
+    bg = buffer.getGraphics();
+
+    // Add background color.
+    bg.setColor(Color.WHITE);
+    bg.fillRect(0, 0, getWidth(), getHeight());
+
+    // GameOfLife object
+    GameOfLife gol = new GameOfLife();
+
+    //-------------------------------Draw Section-------------------------------
+
+    // Draw the image from buffer onto canvas
+    g.drawImage(buffer, 0, 0, null);
   }
 
   // Main function.
